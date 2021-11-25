34a3d56d88f56314b6f6ac8410427193f5234e5b
Sakib Mahmud <sakib.mahmud@qu.edu.qa>
2021-11-25 14:09:58 +0300

    Add files via upload

---
+++ Codes/DenseNet_2DCNN.py
@@ -0,0 +1,217 @@
+"""DenseNet 2DCNN in Tensorflow-Keras
+Reference: Densely Connected Convolutional Networks [https://arxiv.org/abs/1608.06993]
+"""
+
+
+import tensorflow as tf
+
+
+def Conv_2D_Block(inputs, model_width, kernel, strides):
+    # 2D Convolutional Block with BatchNormalization
+    x = tf.keras.layers.Conv2D(model_width, kernel, strides=strides, padding="same", kernel_initializer="he_normal")(inputs)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+
+    return x
+
+
+def stem(inputs, num_filters):
+    # Construct the Stem Convolution Group
+    # inputs : input vector
+    conv = Conv_2D_Block(inputs, num_filters, (7, 7), (2, 2))
+    if conv.shape[1] <= 2:
+        pool = tf.keras.layers.MaxPooling2D(pool_size=(1, 1), strides=(2, 2), padding="same")(conv)
+    else:
+        pool = tf.keras.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="same")(conv)
+
+    return pool
+
+
+def conv_block(x, num_filters, bottleneck=True):
+    # Construct Block of Convolutions without Pooling
+    # x        : input into the block
+    # n_filters: number of filters
+    if bottleneck:
+        num_filters_bottleneck = num_filters * 4
+        x = Conv_2D_Block(x, num_filters_bottleneck, (1, 1), (1, 1))
+
+    out = Conv_2D_Block(x, num_filters, (3, 3), (1, 1))
+
+    return out
+
+
+def dense_block(x, num_filters, num_layers, bottleneck=True):
+    for i in range(num_layers):
+        cb = conv_block(x, num_filters, bottleneck=bottleneck)
+        x = tf.keras.layers.concatenate([x, cb], axis=-1)
+
+    return x
+
+
+def transition_block(inputs, num_filters):
+    x = Conv_2D_Block(inputs, num_filters, (1, 1), (2, 2))
+    if x.shape[1] <= 2:
+        x = tf.keras.layers.AveragePooling2D(pool_size=(1, 1), strides=(2, 2), padding="same")(x)
+    else:
+        x = tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding="same")(x)
+
+    return x
+
+
+def classifier(inputs, class_number):
+    # Construct the Classifier Group
+    # inputs       : input vector
+    # class_number : number of output classes
+    out = tf.keras.layers.Dense(class_number, activation='softmax')(inputs)
+
+    return out
+
+
+def regressor(inputs, feature_number):
+    # Construct the Regressor Group
+    # inputs         : input vector
+    # feature_number : number of output features
+    out = tf.keras.layers.Dense(feature_number, activation='linear')(inputs)
+
+    return out
+
+
+class DenseNet:
+    def __init__(self, length, width, num_channel, num_filters, problem_type='Regression',
+                 output_nums=1, pooling='avg', dropout_rate=False, bottleneck=True):
+        # length: Input Signal Length
+        # model_depth: Depth of the Model
+        # model_width: Width of the Model
+        # kernel_size: Kernel or Filter Size of the Input Convolutional Layer
+        # num_channel: Number of Channels of the Input Predictor Signals
+        # problem_type: Regression or Classification
+        # output_nums: Number of Output Classes in Classification mode and output features in Regression mode
+        # pooling: Choose either 'max' for MaxPooling or 'avg' for Averagepooling
+        # dropout_rate: If turned on, some layers will be dropped out randomly based on the selected proportion
+        # bottleneck: Option for Turning the Bottleneck structure ON or OFF
+        self.length = length
+        self.width = width
+        self.num_channel = num_channel
+        self.num_filters = num_filters
+        self.problem_type = problem_type
+        self.output_nums = output_nums
+        self.pooling = pooling
+        self.dropout_rate = dropout_rate
+        self.bottleneck = bottleneck
+
+    def MLP(self, x):
+        outputs = []
+        if self.pooling == 'avg':
+            x = tf.keras.layers.GlobalAveragePooling2D()(x)
+        elif self.pooling == 'max':
+            x = tf.keras.layers.GlobalMaxPooling2D()(x)
+        # Final Dense Outputting Layer for the outputs
+        x = tf.keras.layers.Flatten(name='flatten')(x)
+        if self.dropout_rate:
+            x = tf.keras.layers.Dropout(self.dropout_rate)(x)
+        # Problem Types
+        if self.problem_type == 'Classification':
+            # The Classifier for n classes
+            class_number = self.output_nums
+            outputs = classifier(x, class_number)
+        elif self.problem_type == 'Regression':
+            # The Regressor [Default]
+            feature_number = self.output_nums
+            outputs = regressor(x, feature_number)
+
+        return outputs
+
+    def DenseNet121(self):
+        inputs = tf.keras.Input((self.length, self.width, self.num_channel))  # The input tensor
+        stem_block = stem(inputs, self.num_filters)  # The Stem Convolution Group
+        Dense_Block_1 = dense_block(stem_block, self.num_filters * 2, 6, bottleneck=self.bottleneck)
+        Transition_Block_1 = transition_block(Dense_Block_1, self.num_filters)
+        Dense_Block_2 = dense_block(Transition_Block_1, self.num_filters * 4, 12, bottleneck=self.bottleneck)
+        Transition_Block_2 = transition_block(Dense_Block_2, self.num_filters)
+        Dense_Block_3 = dense_block(Transition_Block_2, self.num_filters * 8, 24, bottleneck=self.bottleneck)
+        Transition_Block_3 = transition_block(Dense_Block_3, self.num_filters)
+        Dense_Block_4 = dense_block(Transition_Block_3, self.num_filters * 16, 16, bottleneck=self.bottleneck)
+        outputs = self.MLP(Dense_Block_4)
+        # Instantiate the Model
+        model = tf.keras.Model(inputs, outputs)
+
+        return model
+
+    def DenseNet161(self):
+        inputs = tf.keras.Input((self.length, self.width, self.num_channel))  # The input tensor
+        stem_block = stem(inputs, self.num_filters)  # The Stem Convolution Group
+        Dense_Block_1 = dense_block(stem_block, self.num_filters * 2, 6, bottleneck=self.bottleneck)
+        Transition_Block_1 = transition_block(Dense_Block_1, self.num_filters * 2)
+        Dense_Block_2 = dense_block(Transition_Block_1, self.num_filters * 4, 12, bottleneck=self.bottleneck)
+        Transition_Block_2 = transition_block(Dense_Block_2, self.num_filters * 4)
+        Dense_Block_3 = dense_block(Transition_Block_2, self.num_filters * 8, 36, bottleneck=self.bottleneck)
+        Transition_Block_3 = transition_block(Dense_Block_3, self.num_filters * 8)
+        Dense_Block_4 = dense_block(Transition_Block_3, self.num_filters * 16, 24, bottleneck=self.bottleneck)
+        outputs = self.MLP(Dense_Block_4)
+        # Instantiate the Model
+        model = tf.keras.Model(inputs, outputs)
+
+        return model
+
+    def DenseNet169(self):
+        inputs = tf.keras.Input((self.length, self.width, self.num_channel))  # The input tensor
+        stem_block = stem(inputs, self.num_filters)  # The Stem Convolution Group
+        Dense_Block_1 = dense_block(stem_block, self.num_filters * 2, 6, bottleneck=self.bottleneck)
+        Transition_Block_1 = transition_block(Dense_Block_1, self.num_filters * 2)
+        Dense_Block_2 = dense_block(Transition_Block_1, self.num_filters * 4, 12, bottleneck=self.bottleneck)
+        Transition_Block_2 = transition_block(Dense_Block_2, self.num_filters * 4)
+        Dense_Block_3 = dense_block(Transition_Block_2, self.num_filters * 8, 32, bottleneck=self.bottleneck)
+        Transition_Block_3 = transition_block(Dense_Block_3, self.num_filters * 8)
+        Dense_Block_4 = dense_block(Transition_Block_3, self.num_filters * 16, 32, bottleneck=self.bottleneck)
+        outputs = self.MLP(Dense_Block_4)
+        # Instantiate the Model
+        model = tf.keras.Model(inputs, outputs)
+
+        return model
+
+    def DenseNet201(self):
+        inputs = tf.keras.Input((self.length, self.width, self.num_channel))  # The input tensor
+        stem_block = stem(inputs, self.num_filters)  # The Stem Convolution Group
+        Dense_Block_1 = dense_block(stem_block, self.num_filters * 2, 6, bottleneck=self.bottleneck)
+        Transition_Block_1 = transition_block(Dense_Block_1, self.num_filters)
+        Dense_Block_2 = dense_block(Transition_Block_1, self.num_filters * 4, 12, bottleneck=self.bottleneck)
+        Transition_Block_2 = transition_block(Dense_Block_2, self.num_filters)
+        Dense_Block_3 = dense_block(Transition_Block_2, self.num_filters * 8, 48, bottleneck=self.bottleneck)
+        Transition_Block_3 = transition_block(Dense_Block_3, self.num_filters)
+        Dense_Block_4 = dense_block(Transition_Block_3, self.num_filters * 16, 32, bottleneck=self.bottleneck)
+        outputs = self.MLP(Dense_Block_4)
+        # Instantiate the Model
+        model = tf.keras.Model(inputs, outputs)
+
+        return model
+
+    def DenseNet264(self):
+        inputs = tf.keras.Input((self.length, self.width, self.num_channel))  # The input tensor
+        stem_block = stem(inputs, self.num_filters)  # The Stem Convolution Group
+        Dense_Block_1 = dense_block(stem_block, self.num_filters * 2, 6, bottleneck=self.bottleneck)
+        Transition_Block_1 = transition_block(Dense_Block_1, self.num_filters * 2)
+        Dense_Block_2 = dense_block(Transition_Block_1, self.num_filters * 4, 12, bottleneck=self.bottleneck)
+        Transition_Block_2 = transition_block(Dense_Block_2, self.num_filters * 4)
+        Dense_Block_3 = dense_block(Transition_Block_2, self.num_filters * 8, 64, bottleneck=self.bottleneck)
+        Transition_Block_3 = transition_block(Dense_Block_3, self.num_filters * 8)
+        Dense_Block_4 = dense_block(Transition_Block_3, self.num_filters * 16, 48, bottleneck=self.bottleneck)
+        outputs = self.MLP(Dense_Block_4)
+        # Instantiate the Model
+        model = tf.keras.Model(inputs, outputs)
+
+        return model
+
+
+if __name__ == '__main__':
+    # Configurations
+    length = 224  # Length of each Image
+    width = 224  # Width of each Image
+    model_name = 'DenseNet201'  # DenseNet Models
+    model_width = 16 # Width of the Initial Layer, subsequent layers start from here
+    num_channel = 1  # Number of Input Channels in the Model
+    problem_type = 'Regression' # Classification or Regression
+    output_nums = 1  # Number of Class for Classification Problems, always '1' for Regression Problems
+    #
+    Model = DenseNet(length, width, num_channel, model_width, problem_type=problem_type, output_nums=output_nums, pooling='avg', dropout_rate=False, bottleneck=True).DenseNet201()
+    Model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0003), loss=tf.keras.losses.MeanAbsoluteError(), metrics=tf.keras.metrics.MeanSquaredError())
+    Model.summary()
